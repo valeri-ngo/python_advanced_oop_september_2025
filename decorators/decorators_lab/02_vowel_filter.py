3d88c5938eb0b5c6a416b9b4ecc08334c65af277
valeri_ngo <sacredlie@abv.bg>
2025-11-23 18:44:19 +0200

    Decorators lab.

---
+++ decorators/decorators_lab/02_vowel_filter.py
@@ -0,0 +1,13 @@
+def vowel_filter(function):
+
+    def wrapper():
+        letters = function()
+        return [el for el in letters if el.lower() in 'aeiouy']
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
